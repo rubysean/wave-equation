1492f1304fce088e71af2dcac2a5373d68b35c5e
rubysean <rubysean@khu.ac.kr>
2025-11-23 19:45:02 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,10 @@
 import sympy as sp
 
+x,y  =  sp.symbols('x y')
+u = sp.Function('u')(x,y)
+
+pde = sp.Eq(x * sp.diff(u,x) -3 * y * sp.diff(u,y), 2*x**2 * u)
+
+solution = sp.pdsolve(pde)
+
+print(solution)
