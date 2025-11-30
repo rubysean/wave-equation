e9fb1d8357e1496661e613bd376688402694464e
rubysean <rubysean@khu.ac.kr>
2025-11-30 19:24:09 +0900

    Update final wave simulation.py

--- final wave simulation.py
+++ final wave simulation.py
@@ -1,39 +1,31 @@
 import numpy as np
-import matplotlib.plot as plt
+import matplotlib.pyplot as plt
 
 def I(x,y):
-    
-    return 0.2*np.exp(-((x-1)**2/0.1 + (y-1)**2/0.1))
+    return 0.2*np.exp(-((x-2.5)**2/0.1 + (y-2.5)**2/0.1))
 
 def V(x,y):
     return 0
   
 def celer(x,y):
-  
     return 1
     
-loop_exec = 1 # Processing loop execution flag
 
-bound_cond = int(input('cond:'))  #Boundary cond 1 : Dirichlet, 2 : Neumann, 3 Mur
+bound_cond = 1
 
-if bound_cond not in [1,2,3]:
-    loop_exec = 0
-    print("Please choose a correct boundary condition")
+L_x = 5 
+dx = 0.05 
+N_x = int(L_x/dx) 
+X = np.linspace(0,L_x,N_x+1)
+L_y = 5
+dy = 0.05 
+N_y = int(L_y/dy) 
+Y = np.linspace(0,L_y,N_y+1) 
 
-L_x = 5 #Range of the domain according to x [m]
-dx = 0.05 #Infinitesimal distance in the x direction
-N_x = int(L_x/dx) #Points number of the spatial mesh in the x direction
-X = np.linspace(0,L_x,N_x+1) #Spatial array in the x direction
-
-L_y = 5 #Range of the domain according to y [m]
-dy = 0.05 #Infinitesimal distance in the x direction
-N_y = int(L_y/dy) #Points number of the spatial mesh in the y direction
-Y = np.linspace(0,L_y,N_y+1) #Spatial array in the y direction
-
-L_t = 5 #Duration of simulation [s]
-dt = dt = 0.3*min(dx, dy)   #Infinitesimal time with CFL (Courant–Friedrichs–Lewy condition)
-N_t = int(L_t/dt) #Points number of the temporal mesh
-T = np.linspace(0,L_t,N_t+1) #Temporal array
+L_t = 5 
+dt = dt = 0.3*min(dx, dy)   
+N_t = int(L_t/dt) 
+T = np.linspace(0,L_t,N_t+1)
 
 c = np.zeros((N_x+1,N_y+1), float)
 for i in range(0,N_x+1):
@@ -42,10 +34,65 @@ for i in range(0,N_x+1):
 
 Cx2 = (dt/dx)**2
 Cy2 = (dt/dy)**2 
-CFL_1 = dt/dy*c[:,0]
-CFL_2 = dt/dy*c[:,N_y]
-CFL_3 = dt/dx*c[0,:]
-CFL_4 =dt/dx*c[N_x,:]
+
+if True:
+    U = np.zeros((N_x+1,N_x+1,N_t+1),float) 
+
+    u_nm1 = np.zeros((N_x+1,N_y+1),float)   #u_{i,j}^{n-1}
+    u_n = np.zeros((N_x+1,N_y+1),float)     # u_{i,j}^{n}
+    u_np1 = np.zeros((N_x+1,N_y+1),float)  # u_{i,j}^{n+1}
+    V_init = np.zeros((N_x+1,N_y+1),float)
+    q = np.zeros((N_x+1, N_y+1), float)
+
+    #t = 0
+    for i in range(0, N_x+1):
+        for j in range(0, N_y+1):
+            q[i,j] = c[i,j]**2
+    
+    for i in range(0, N_x+1):
+        for j in range(0, N_y+1):
+            u_n[i,j] = I(X[i],Y[j])
+            
+    for i in range(0, N_x+1):
+        for j in range(0, N_y+1):
+            V_init[i,j] = V(X[i],Y[j])
+    
+    U[:,:,0] = u_n.copy()
+
+    # t = 1
+    #without boundary cond
+    u_np1[1:N_x,1:N_y] = 2*u_n[1:N_x,1:N_y] - (u_n[1:N_x,1:N_y] - 2*dt*V_init[1:N_x,1:N_y]) + Cx2*q[1:N_x, 1: N_y]*(u_n[2:N_x+1, 1: N_y] - 2*u_n[1:N_x, 1: N_y] + u_n[0:N_x-1, 1: N_y]) + Cy2*q[1:N_x, 1: N_y]*(u_n[1:N_x, 2: N_y+1] -2*u_n[1:N_x, 1: N_y] + u_n[1:N_x, 0: N_y-1]) 
+
+    #boundary conditions
+    if bound_cond == 1:
+        #Dirichlet bound cond
+        u_np1[0,:] = 0
+        u_np1[-1,:] = 0
+        u_np1[:,0] = 0
+        u_np1[:,-1] = 0
+
+    u_nm1 = u_n.copy()
+    u_n = u_np1.copy()
+    U[:,:,1] = u_n.copy()
+    
+    for n in range(2, N_t):
+        
+        #calculation at step j+1  
+        #without boundary cond           
+        u_np1[1:N_x,1:N_y] = 2*u_n[1:N_x,1:N_y] - u_nm1[1:N_x,1:N_y] + Cx2*q[1:N_x, 1: N_y]*(u_n[2:N_x+1, 1: N_y] - 2*u_n[1:N_x, 1: N_y] + u_n[0:N_x-1, 1: N_y]) + Cy2*q[1:N_x, 1: N_y]*(u_n[1:N_x, 2: N_y+1] -2*u_n[1:N_x, 1: N_y] + u_n[1:N_x, 0: N_y-1]) 
+        
+        if bound_cond == 1:
+            #Dirichlet bound cond
+            u_np1[0,:] = 0
+            u_np1[-1,:] = 0
+            u_np1[:,0] = 0
+            u_np1[:,-1] = 0
+            
+        u_nm1 = u_n.copy()      
+        u_n = u_np1.copy() 
+        U[:,:,n] = u_n.copy()
+    
+
 
 
 
